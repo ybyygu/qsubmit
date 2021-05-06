1e0e09b7deb0d085488b811ddea54748cba56d39
Wenping Guo <ybyygu@gmail.com>
2021-05-06 14:36:19 +0800

    New watch option to wait for new jobs

--- src/cmd.rs
+++ src/cmd.rs
@@ -11,20 +11,43 @@ struct Cli {
     verbose: gut::cli::Verbosity,
 
     /// The path to queue dir.
-    #[structopt(name = "QUEUE-DIR", long)]
+    #[structopt(name = "QUEUE-DIR", long = "qdir", short = "q")]
     qdir: PathBuf,
+
+    /// Continue to Watch queue dir for new jobs
+    #[structopt(short)]
+    watch: bool,
+}
+
+fn sleep(n: u64) {
+    use std::{thread, time};
+
+    let ten_millis = time::Duration::from_secs(n);
+    let now = time::Instant::now();
+
+    thread::sleep(ten_millis);
 }
 
 pub fn enter_main_loop() -> Result<()> {
     let args = Cli::from_args();
     args.verbose.setup_logger();
 
-    let qdir = &args.qdir;
+    let scan_rate = 2;
     loop {
-        let q = get_next_job_from_qdir(qdir, true)?;
-        let o = execute_job(&q)?;
-        archive_job(&q)?;
-        dbg!(o);
+        match get_next_job_from_qdir(&args.qdir, true) {
+            Ok(q) => {
+                let o = execute_job(dbg!(&q))?;
+                dbg!(o);
+            }
+            Err(e) => {
+                if args.watch {
+                    info!("waiting {} seconds for new job ....", scan_rate);
+                    sleep(scan_rate);
+                } else {
+                    return Err(e);
+                }
+            }
+        }
     }
 }
 // cmd.rs:1 ends here
